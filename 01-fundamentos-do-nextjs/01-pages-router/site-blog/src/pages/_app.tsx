58d7598b599a08401626f9f6806577db84436d7e
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-04-08 13:19:47 -0300

    aula: diferenças entre _app e _document

--- 01-fundamentos-do-nextjs/01-pages-router/site-blog/src/pages/_app.tsx
+++ 01-fundamentos-do-nextjs/01-pages-router/site-blog/src/pages/_app.tsx
@@ -1,6 +1,10 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
+// O _app é renderizado pelo servidor e pelo browser. 
+// Serve como um wrapper  para englobar todos os nossos componentes. 
+// É nela que vamos utilizar nossos providerspor exemplo, quando utilizarmos themes, contexts, global estyles, etc…
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
--- 01-fundamentos-do-nextjs/01-pages-router/site-blog/src/pages/_document.tsx
+++ 01-fundamentos-do-nextjs/01-pages-router/site-blog/src/pages/_document.tsx
@@ -1,5 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+// Já o _document é como se fosse nosso index.html, 
+// é onde vamos controlar a importação de scripts, controlando a estrutura do nosso HTML, metatags, favicon. 
+// Ele só é executado ao lado do servidor.
+
 export default function Document() {
   return (
     <Html lang="pt-BR">
