8bd33ede43cd97977c92bd8bc95080a35a51a632
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-04-09 09:10:48 -0300

    aula: componente active link

---
+++ 01-fundamentos-do-nextjs/01-pages-router/site-blog/src/components/active-link/active-link.tsx
@@ -0,0 +1,31 @@
+import { cn } from "@/lib/utils";
+import Link, { LinkProps } from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+
+type ActiveLinkProps = {
+  children: React.ReactNode;
+} & LinkProps;
+
+export function ActiveLink({ children, href, ...rest }: ActiveLinkProps) {
+  const router = useRouter();
+  const isCurrentPath =
+    router.asPath === href ||
+    router.asPath === rest.as ||
+    router.asPath.startsWith(String(rest.as));
+
+  console.log(router.asPath);
+  console.log(rest.as);
+
+  return (
+    <Link
+      href={href}
+      className={cn(
+        "text-sm font-medium transition-colors hover:text-blue-500",
+        isCurrentPath ? "text-blue-500" : "text-muted-foreground"
+      )}
+    >
+      {children}
+    </Link>
+  );
+}
--- 01-fundamentos-do-nextjs/01-pages-router/site-blog/src/components/header/header.tsx
+++ 01-fundamentos-do-nextjs/01-pages-router/site-blog/src/components/header/header.tsx
@@ -1,39 +1,23 @@
 import { Button } from "../ui/button";
-import { useRouter } from "next/router";
-import { cn } from "@/lib/utils";
 import Link from "next/link";
+import { ActiveLink } from "../active-link";
+import Image from "next/image";
 
 export function Header() {
-  const router = useRouter();
-  const isHomePage = router.pathname === "/";
-  const isBlogPage = router.pathname.startsWith("/blog");
-
   return (
     <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filters]:bg-background/60">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
-          <Link href="/">Logo</Link>
+          <Link href="/">
+            <Image src="/logo.svg" alt="" width={104} height={24} />
+          </Link>
 
           <nav className="flex items-center gap-6">
-            <Link
-              href="/"
-              className={cn(
-                "text-sm font-medium transition-colors hover:text-blue-500",
-                isHomePage ? "text-blue-500" : "text-muted-foreground"
-              )}
-            >
-              Início
-            </Link>
-            <Link
-              href="/blog"
-              className={cn(
-                "text-sm font-medium transition-colors hover:text-blue-500",
-                isBlogPage ? "text-blue-500" : "text-muted-foreground"
-              )}
-            >
-              Blog
-            </Link>
-            <Button variant="secondary">Começar</Button>
+            <ActiveLink href="/">Início</ActiveLink>
+            <ActiveLink href="/blog">Blog</ActiveLink>
+            <Button variant="secondary" asChild>
+              <Link href="/comecar">Começar</Link>
+            </Button>
           </nav>
         </div>
       </div>
