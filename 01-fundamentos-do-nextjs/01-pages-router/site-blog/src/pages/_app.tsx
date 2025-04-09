37c8e57cbf4da98a34a343ec5bc72b14b6ae2300
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-04-09 09:21:36 -0300

    aula: layout

--- 01-fundamentos-do-nextjs/01-pages-router/site-blog/src/pages/_app.tsx
+++ 01-fundamentos-do-nextjs/01-pages-router/site-blog/src/pages/_app.tsx
@@ -1,18 +1,21 @@
+import { Layout } from "@/components/layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
-// O _app é renderizado pelo servidor e pelo browser. 
-// Serve como um wrapper  para englobar todos os nossos componentes. 
+// O _app é renderizado pelo servidor e pelo browser.
+// Serve como um wrapper  para englobar todos os nossos componentes.
 // É nela que vamos utilizar nossos providerspor exemplo, quando utilizarmos themes, contexts, global estyles, etc…
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-    <Head>
-      <title>Site.Set | Venda de produtos digitais</title>
-    </Head>
-    <Component {...pageProps} />
+      <Head>
+        <title>Site.Set | Venda de produtos digitais</title>
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </>
   );
 }
--- 01-fundamentos-do-nextjs/01-pages-router/site-blog/src/pages/index.tsx
+++ 01-fundamentos-do-nextjs/01-pages-router/site-blog/src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { Header } from "@/components/header";
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -15,8 +14,6 @@ export default function Home() {
   return (
     <div
       className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <Header />
-    </div>
+    ></div>
   );
 }
