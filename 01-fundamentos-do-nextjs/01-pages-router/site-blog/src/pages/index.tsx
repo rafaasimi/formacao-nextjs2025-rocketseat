2e05cbfdde540f21354bc41c9c2feba3b33cb07f
Rafael Simionato Zorzenon <rafaasimi@hotmail.com>
2025-04-09 08:33:11 -0300

    aula: componente header

--- 01-fundamentos-do-nextjs/01-pages-router/site-blog/src/pages/index.tsx
+++ 01-fundamentos-do-nextjs/01-pages-router/site-blog/src/pages/index.tsx
@@ -1,8 +1,22 @@
-import { Button } from "@/components/ui/button";
+import { Header } from "@/components/header";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export default function Home() {
-  return <div>Home
-    <Button variant="default">Button</Button>
-
-  </div>;
+  return (
+    <div
+      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
+    >
+      <Header />
+    </div>
+  );
 }
