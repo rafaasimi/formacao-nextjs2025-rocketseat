9a677cbb42d4da95379012e3b1956cc24e4f1c32
Rafael Simionato <rafaasimi@hotmail.com>
2026-07-23 09:55:19 -0300

    feat: entendendo roteamento no nextjs

---
+++ 01-fundamentos-do-nextjs/site-blog/src/pages/blog/posts/[slug].tsx
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+
+export default function PostPage() {
+    const router = useRouter();
+    const { slug } = router.query;
+
+    return (
+        <div>Post: {slug}</div>
+    )
+}
