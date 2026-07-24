dd610f343c50a33af655e6a46083d6061e5fcdf5
Rafael Simionato <rafaasimi@hotmail.com>
2026-07-24 12:23:53 -0300

    feat: layout

--- 01-fundamentos-do-nextjs/site-blog/src/pages/index.tsx
+++ 01-fundamentos-do-nextjs/site-blog/src/pages/index.tsx
@@ -16,7 +16,6 @@ export default function HomePage() {
     <div
       className={`${geistSans.className} ${geistMono.className} flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
     >
-      <Header />
     </div>
   );
 }
