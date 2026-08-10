bf5d17be8c55ee31cd0f2fe59fadd75d08aab5c4
Rafael Simionato <rafaasimi@hotmail.com>
2026-08-10 19:48:07 -0300

    feat: melhorando o seo da pagina do post

--- 01-fundamentos-do-nextjs/site-blog/src/app/blog/[slug]/page.tsx
+++ 01-fundamentos-do-nextjs/site-blog/src/app/blog/[slug]/page.tsx
@@ -1,9 +1,45 @@
 import { Post } from "@/templates/blog/post-page";
 import { allPosts } from "contentlayer/generated";
+import { Metadata } from "next/dist/lib/metadata/types/metadata-interface";
 import { notFound } from "next/navigation";
 
 type BlogPageProps = {
     params: Promise<{ slug: string }>
+}
+
+export async function generateMetadata({ params }: BlogPageProps): Promise<Metadata> {
+    const { slug } = await params;
+    const post = allPosts.find((post) => post.slug === slug);
+
+    if (!post) {
+        return {}
+    }
+
+    return (
+        {
+            title: post.title,
+            description: post.description,
+            authors: [
+                {
+                    name: post.author.name,
+                }
+            ],
+            robots: {
+                index: true,
+                follow: true,
+            },
+            openGraph: {
+                title: post.title,
+                description: post.description,
+                images: [
+                    {
+                        url: post.image,
+                        alt: post.title,
+                    }
+                ]
+            }
+        }
+    )
 }
 
 export const revalidate = 3600; // 1 hora
