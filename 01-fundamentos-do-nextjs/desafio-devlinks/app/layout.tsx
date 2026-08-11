8a786abfc77e4033e35c364a02c5149dcb5e3c06
Rafael Simionato <rafaasimi@hotmail.com>
2026-08-11 07:51:53 -0300

    feat: arquivos iniciais

---
+++ 01-fundamentos-do-nextjs/desafio-devlinks/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const interSans = Inter({
+  variable: "--font-inter-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Rafael Simionato - DevLinks",
+  description: "DevLinks é um agregador de links responsivo e com troca de tema que você pode usar como cartão de visitas nas suas redes sociais.",
+};
+
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <html
+      lang="pt-BR"
+      className={`${interSans.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
