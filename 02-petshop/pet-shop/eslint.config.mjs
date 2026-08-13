ddc080c0db08302b697b0f50c069ca1b86e2b6ad
Rafael Simionato <rafaasimi@hotmail.com>
2026-08-13 11:16:44 -0300

    feat: configurado prettier, eslint e lefthook

--- 02-petshop/pet-shop/eslint.config.mjs
+++ 02-petshop/pet-shop/eslint.config.mjs
@@ -1,18 +1,26 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier/flat';
 
-const eslintConfig = defineConfig([
+export default defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  prettier,
   globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    'src/generated/prisma/**',
+    'pgdata/**',
+    'node_modules/**',
   ]),
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    rules: {
+      'arrow-body-style': 'off',
+      'prefer-arrow-callback': 'off',
+    },
+  },
 ]);
-
-export default eslintConfig;
