90678d352923cfb18e72574c86106047ee0532b4
Rafael Simionato <rafaasimi@hotmail.com>
2026-09-10 08:42:44 -0300

    feat: efeitos de hover e focus

--- 03-autenticacao-e-api-externa-no-nextjs/board/src/app/page.tsx
+++ 03-autenticacao-e-api-externa-no-nextjs/board/src/app/page.tsx
@@ -1,6 +1,7 @@
 import { ArchiveIcon, MessageCircleIcon, ThumbsUpIcon } from "lucide-react";
 import { Section } from "@/components/section";
 import { Card } from "@/components/card";
+import { Button } from "@/components/button";
 
 export default function Home() {
   return (
@@ -25,15 +26,15 @@ export default function Home() {
                 <Card.Title>Implementar autenticação</Card.Title>
               </Card.Header>
               <Card.Footer>
-                <button type="button" className="text-navy-100 flex items-center gap-2 rounded-lg px-2.5 py-1 bg-navy-600 cursor-pointer">
+                <Button>
                   <ThumbsUpIcon className="size-3" />
                   <span className="text-sm">12</span>
-                </button>
+                </Button>
 
-                <button type="button" className="text-navy-100 flex items-center gap-2 rounded-lg px-2.5 py-1 bg-navy-600 cursor-pointer">
+                <Button>
                   <MessageCircleIcon className="size-3" />
                   <span className="text-sm">6</span>
-                </button>
+                </Button>
               </Card.Footer>
             </Card.Root>
           </Section.Content>
