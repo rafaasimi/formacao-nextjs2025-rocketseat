0a6fe36c87508f3325f0044879efe22c4c687647
Rafael Simionato <rafaasimi@hotmail.com>
2026-09-11 08:24:37 -0300

    feat: roteamentos e layouts no nextjs

---
+++ 03-autenticacao-e-api-externa-no-nextjs/board/src/app/(board)/layout.tsx
@@ -0,0 +1,9 @@
+export default function RootLayout({ children }: LayoutProps<"/">) {
+  return (
+    <div className="max-w-[1620px] w-full mx-auto p-10 flex flex-col gap-8 h-dvh">
+      <div>Header</div>
+
+      {children}
+    </div>
+  );
+}
---
+++ 03-autenticacao-e-api-externa-no-nextjs/board/src/app/(board)/page.tsx
@@ -0,0 +1,42 @@
+import { ArchiveIcon, MessageCircleIcon, ThumbsUpIcon } from "lucide-react";
+import { Section } from "@/components/section";
+import { Card } from "@/components/card";
+import { Button } from "@/components/button";
+
+export default function Home() {
+  return (
+    <main className="grid grid-cols-4 gap-5 flex-1 items-stretch">
+
+      <Section.Root>
+        <Section.Header>
+          <Section.Title>
+            <ArchiveIcon className="size-3" />
+            Backlog
+          </Section.Title>
+          <Section.IssueCount>5</Section.IssueCount>
+        </Section.Header>
+
+        <Section.Content>
+          <Card.Root>
+            <Card.Header>
+              <Card.Number>CA01-0001</Card.Number>
+              <Card.Title>Implementar autenticação</Card.Title>
+            </Card.Header>
+            <Card.Footer>
+              <Button>
+                <ThumbsUpIcon className="size-3" />
+                <span className="text-sm">12</span>
+              </Button>
+
+              <Button>
+                <MessageCircleIcon className="size-3" />
+                <span className="text-sm">6</span>
+              </Button>
+            </Card.Footer>
+          </Card.Root>
+        </Section.Content>
+      </Section.Root>
+
+    </main>
+  );
+}
