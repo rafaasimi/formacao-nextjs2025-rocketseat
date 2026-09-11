6462740bc867c42651b574ebc55a8d4473d96d65
Rafael Simionato <rafaasimi@hotmail.com>
2026-09-11 08:51:01 -0300

    feat: estrutura do cabecalho

--- 03-autenticacao-e-api-externa-no-nextjs/board/src/app/(board)/layout.tsx
+++ 03-autenticacao-e-api-externa-no-nextjs/board/src/app/(board)/layout.tsx
@@ -1,7 +1,9 @@
+import { Header } from "./header";
+
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <div className="max-w-[1620px] w-full mx-auto p-10 flex flex-col gap-8 h-dvh">
-      <div>Header</div>
+      <Header />
 
       {children}
     </div>
