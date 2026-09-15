9e80fcaa1fd868275744d9db78dbfe5c9714ed91
Rafael Simionato <rafaasimi@hotmail.com>
2026-09-15 08:08:00 -0300

    feat: metadados das paginas

--- 03-autenticacao-e-api-externa-no-nextjs/board/src/app/(board)/page.tsx
+++ 03-autenticacao-e-api-externa-no-nextjs/board/src/app/(board)/page.tsx
@@ -2,6 +2,11 @@ import { ArchiveIcon, MessageCircleIcon, ThumbsUpIcon } from "lucide-react";
 import { Section } from "@/components/section";
 import { Card } from "@/components/card";
 import { Button } from "@/components/button";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Board"
+};
 
 interface BoardProps {
   searchParams: Promise<{
