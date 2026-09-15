1b1e18af5461aa5f9555cd5f3e8a6303f4a00331
Rafael Simionato <rafaasimi@hotmail.com>
2026-09-15 08:22:26 -0300

    feat: client e server componentes

---
+++ 03-autenticacao-e-api-externa-no-nextjs/board/src/app/(board)/header/search-input.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { Input } from "@/components/input";
+import { SearchIcon } from "lucide-react";
+import { debounce, parseAsString, useQueryState } from "nuqs";
+import { ChangeEvent } from "react";
+
+export function SearchInput() {
+    const [search, setSearch] = useQueryState("q", parseAsString.withDefault(""));
+
+    function handleSearchUpdate(event: ChangeEvent<HTMLInputElement>) {
+        setSearch(event.target.value, {
+            limitUrlUpdates: event.target.value !== '' ? debounce(500) : undefined,
+        });
+    }
+
+    return (
+        <div className="relative">
+            <SearchIcon className="absolute size-4 text-navy-200 left-2.5 top-1/2 -translate-y-1/2 pointer-events-none" />
+            <Input
+                type="text"
+                placeholder="Search for features..."
+                className="w-67.5 pl-8"
+                value={search}
+                onChange={handleSearchUpdate}
+            />
+        </div>
+    );
+}
