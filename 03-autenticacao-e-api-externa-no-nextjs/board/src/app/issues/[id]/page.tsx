c8361c8b44172209eb00c6f4534d4ae75c9e61de
Rafael Simionato <rafaasimi@hotmail.com>
2026-09-16 15:53:26 -0300

    feat: listagem de comentarios

--- 03-autenticacao-e-api-externa-no-nextjs/board/src/app/issues/[id]/page.tsx
+++ 03-autenticacao-e-api-externa-no-nextjs/board/src/app/issues/[id]/page.tsx
@@ -3,6 +3,7 @@ import { getIssue } from "@/http/get-issue";
 import { ArchiveIcon, MoveLeftIcon, ThumbsUpIcon } from "lucide-react";
 import { Metadata } from "next";
 import Link from "next/link";
+import { IssueCommentsList } from "./issue-comments/issue-comments-list";
 
 interface IssuePageProps {
     params: Promise<{
@@ -52,6 +53,16 @@ export default async function IssuePage({ params }: IssuePageProps) {
             <div className="space-y-2">
                 <h1 className="font-semibold text-2xl">{issue.title}</h1>
                 <p className="text-navy-100 text-sm leading-relaxed">{issue.description}</p>
+            </div>
+
+            <div className="flex flex-col gap-2">
+                <span className="font-semibold">Comments</span>
+
+                <form action="">Form</form>
+
+                <div className="mt-3">
+                    <IssueCommentsList issueId={issue.id} />
+                </div>
             </div>
         </main>
     );
