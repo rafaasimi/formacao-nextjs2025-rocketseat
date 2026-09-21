86e798b1a0e077759f8673c0acfb3b2df678b9cb
Rafael Simionato <rafaasimi@hotmail.com>
2026-09-21 07:54:33 -0300

    feat: setup do react query

--- 03-autenticacao-e-api-externa-no-nextjs/board/src/app/layout.tsx
+++ 03-autenticacao-e-api-externa-no-nextjs/board/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from 'next/font/google';
 import { NuqsAdapter } from "nuqs/adapters/next/app";
+import { ReactQueryProvider } from "@/lib/react-query";
 
 const interFont = Inter({ subsets: ['latin'] });
 
@@ -17,9 +18,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html lang="pt-BR" className={interFont.className}>
       <body className="bg-navy-950 text-navy-50 antialiased">
-        <NuqsAdapter>
-          {children}
-        </NuqsAdapter>
+        <ReactQueryProvider>
+          <NuqsAdapter>
+            {children}
+          </NuqsAdapter>
+        </ReactQueryProvider>
       </body>
     </html>
   );
