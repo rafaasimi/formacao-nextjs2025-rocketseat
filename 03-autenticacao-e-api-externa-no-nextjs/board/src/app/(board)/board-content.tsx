569fa54de7b8e51859962e439ede95f2fa013c7a
Rafael Simionato <rafaasimi@hotmail.com>
2026-09-23 08:50:15 -0300

    feat: carregando interacoes no board

---
+++ 03-autenticacao-e-api-externa-no-nextjs/board/src/app/(board)/board-content.tsx
@@ -0,0 +1,219 @@
+'use client';
+
+import { Button } from "@/components/button";
+import { Card } from "@/components/card";
+import { Section } from "@/components/section";
+import { ArchiveIcon, ThumbsUpIcon, MessageCircleIcon } from "lucide-react";
+import z from "zod";
+import { IssuesListResponseSchema } from "@/api/routes/list-issues";
+import { useQuery } from "@tanstack/react-query";
+import { getIssueInteractions } from "@/http/get-issue-interactions";
+import { LikeButton } from "@/components/like-button";
+
+interface BoardContentProps {
+    issues: z.infer<typeof IssuesListResponseSchema>;
+}
+
+export function BoardContent({ issues }: BoardContentProps) {
+    const { backlog, todo, in_progress, done } = issues;
+    const allIssuesIds = [...backlog, ...todo, ...in_progress, ...done].map(issue => issue.id);
+
+    const { data: interactionsData, isLoading: isLoadingInteractions } = useQuery({
+        queryKey: ['issue-likes', allIssuesIds.sort().join(',')],
+        queryFn: () => getIssueInteractions({ issueIds: allIssuesIds })
+    });
+
+    const interactions = interactionsData && new Map<string, { isLiked: boolean; likesCount: number; }>(interactionsData.interactions.map(interaction => [
+        interaction.issueId,
+        {
+            isLiked: interaction.isLiked,
+            likesCount: interaction.likesCount,
+        }
+    ]));
+
+    return (
+        <main className="grid grid-cols-4 gap-5 flex-1 items-stretch">
+
+            <Section.Root>
+                <Section.Header>
+                    <Section.Title>
+                        <ArchiveIcon className="size-3" />
+                        Backlog
+                    </Section.Title>
+                    <Section.IssueCount>{backlog.length}</Section.IssueCount>
+                </Section.Header>
+
+                <Section.Content>
+                    {backlog.length === 0 ? (
+                        <div className="flex items-center justify-center py-8 text-center">
+                            <p className="text-sm text-navy-300">
+                                No issues matching your filters
+                            </p>
+                        </div>
+                    ) : (
+                        backlog.map(issue => {
+                            const interaction = interactions?.get(issue.id);
+
+                            return (
+                                <Card.Root href={`/issues/${issue.id}`} key={issue.id}>
+                                    <Card.Header>
+                                        <Card.Number>CA01-{issue.issueNumber}</Card.Number>
+                                        <Card.Title>{issue.title}</Card.Title>
+                                    </Card.Header>
+                                    <Card.Footer>
+                                        <LikeButton
+                                            issueId={issue.id}
+                                            initialLikes={interaction?.likesCount ?? 0}
+                                            initialLiked={interaction?.isLiked ?? false}
+                                        />
+
+                                        <Button>
+                                            <MessageCircleIcon className="size-3" />
+                                            <span className="text-sm">{issue.comments}</span>
+                                        </Button>
+                                    </Card.Footer>
+                                </Card.Root>
+                            );
+                        })
+                    )}
+                </Section.Content>
+            </Section.Root>
+
+            <Section.Root>
+                <Section.Header>
+                    <Section.Title>
+                        <ArchiveIcon className="size-3" />
+                        To-do
+                    </Section.Title>
+                    <Section.IssueCount>{todo.length}</Section.IssueCount>
+                </Section.Header>
+
+                <Section.Content>
+                    {todo.length === 0 ? (
+                        <div className="flex items-center justify-center py-8 text-center">
+                            <p className="text-sm text-navy-300">
+                                No issues matching your filters
+                            </p>
+                        </div>
+                    ) : (
+                        todo.map(issue => {
+                            const interaction = interactions?.get(issue.id);
+
+                            return (
+                                <Card.Root href={`/issues/${issue.id}`} key={issue.id}>
+                                    <Card.Header>
+                                        <Card.Number>CA01-{issue.issueNumber}</Card.Number>
+                                        <Card.Title>{issue.title}</Card.Title>
+                                    </Card.Header>
+                                    <Card.Footer>
+                                        <LikeButton
+                                            issueId={issue.id}
+                                            initialLikes={interaction?.likesCount ?? 0}
+                                            initialLiked={interaction?.isLiked ?? false}
+                                        />
+
+                                        <Button>
+                                            <MessageCircleIcon className="size-3" />
+                                            <span className="text-sm">{issue.comments}</span>
+                                        </Button>
+                                    </Card.Footer>
+                                </Card.Root>
+                            );
+                        })
+                    )}
+                </Section.Content>
+            </Section.Root>
+
+            <Section.Root>
+                <Section.Header>
+                    <Section.Title>
+                        <ArchiveIcon className="size-3" />
+                        In progress
+                    </Section.Title>
+                    <Section.IssueCount>{in_progress.length}</Section.IssueCount>
+                </Section.Header>
+
+                <Section.Content>
+                    {in_progress.length === 0 ? (
+                        <div className="flex items-center justify-center py-8 text-center">
+                            <p className="text-sm text-navy-300">
+                                No issues matching your filters
+                            </p>
+                        </div>
+                    ) : (
+                        in_progress.map(issue => {
+                            const interaction = interactions?.get(issue.id);
+
+                            return (
+                                <Card.Root href={`/issues/${issue.id}`} key={issue.id}>
+                                    <Card.Header>
+                                        <Card.Number>CA01-{issue.issueNumber}</Card.Number>
+                                        <Card.Title>{issue.title}</Card.Title>
+                                    </Card.Header>
+                                    <Card.Footer>
+                                        <LikeButton
+                                            issueId={issue.id}
+                                            initialLikes={interaction?.likesCount ?? 0}
+                                            initialLiked={interaction?.isLiked ?? false}
+                                        />
+
+                                        <Button>
+                                            <MessageCircleIcon className="size-3" />
+                                            <span className="text-sm">{issue.comments}</span>
+                                        </Button>
+                                    </Card.Footer>
+                                </Card.Root>
+                            );
+                        })
+                    )}
+                </Section.Content>
+            </Section.Root>
+
+            <Section.Root>
+                <Section.Header>
+                    <Section.Title>
+                        <ArchiveIcon className="size-3" />
+                        Done
+                    </Section.Title>
+                    <Section.IssueCount>{done.length}</Section.IssueCount>
+                </Section.Header>
+
+                <Section.Content>
+                    {done.length === 0 ? (
+                        <div className="flex items-center justify-center py-8 text-center">
+                            <p className="text-sm text-navy-300">
+                                No issues matching your filters
+                            </p>
+                        </div>
+                    ) : (
+                        done.map(issue => {
+                            const interaction = interactions?.get(issue.id);
+
+                            return (
+                                <Card.Root href={`/issues/${issue.id}`} key={issue.id}>
+                                    <Card.Header>
+                                        <Card.Number>CA01-{issue.issueNumber}</Card.Number>
+                                        <Card.Title>{issue.title}</Card.Title>
+                                    </Card.Header>
+                                    <Card.Footer>
+                                        <LikeButton
+                                            issueId={issue.id}
+                                            initialLikes={interaction?.likesCount ?? 0}
+                                            initialLiked={interaction?.isLiked ?? false}
+                                        />
+
+                                        <Button>
+                                            <MessageCircleIcon className="size-3" />
+                                            <span className="text-sm">{issue.comments}</span>
+                                        </Button>
+                                    </Card.Footer>
+                                </Card.Root>
+                            );
+                        })
+                    )}
+                </Section.Content>
+            </Section.Root>
+
+        </main>
+    );
+}
